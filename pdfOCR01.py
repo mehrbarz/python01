822f8ed9605b856b44df687c617aa492928b4b25
Mehr <mehrbarzegar@gmail.com>
2018-06-28 20:37:06 +0200

    file created

--- pdfOCR01.py
+++ pdfOCR01.py
@@ -2,6 +2,22 @@
 
 import os
 import sys
+import json
 
 os.system("sh test-cloud-vision.sh")
+
+f= open('response.json')
+data = json.load(f)
+f.close()
+
+for(k,v) in data.items():
+    print("key: " + k)
+    print("value: " +str(v))
+
+"""
+def getBox(k , v):
+    if 'Box' in k:
+        value = str(v)
+        return value
+"""
 
